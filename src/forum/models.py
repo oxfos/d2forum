3129ddb602553621b94159153260188290e08ca4
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-28 19:32:48 +0100

    Added slug field to forum.models.py and function to automatically convert title into slug.

--- src/forum/models.py
+++ src/forum/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Post(models.Model):
     """A Post the user posts on the forum."""
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a sting representation of the model."""
         return self.title
+
+    def save(self, *args, **kwargs):
+        """Automatically convert title into slug upon saving."""
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
