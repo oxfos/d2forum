87086d2c5d714116726e998859cbb55697a111c6
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-28 20:51:32 +0100

    Added comments to code for more understandability.

--- src/forum/urls.py
+++ src/forum/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'forum'
 urlpatterns = [
+    # Main page with all posts.
     path('', views.posts_list, name="posts_list"),
+    # Post detail.
     path('<int:post_id>/<slug:post_slug>', views.post_detail, name="post_detail"),
 ]
 
--- src/forum/views.py
+++ src/forum/views.py
@@ -3,11 +3,13 @@ from .models import Post
 
 
 def posts_list(request):
+    """View to list all posts."""
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'forum/posts_list.html', context)
 
 def post_detail(request, post_slug, post_id):
+    """View to display post detail."""
     post = Post.objects.get(id=post_id)
     context = {'post': post}
     return render(request, 'forum/post_detail.html', context)
