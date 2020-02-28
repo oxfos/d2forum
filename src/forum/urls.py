e58cf5ef42266b04b6cb52a7deb420655ef67450
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-28 19:34:39 +0100

    Added url, view, template for returning single post.

--- src/forum/urls.py
+++ src/forum/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'forum'
 urlpatterns = [
     path('', views.posts_list, name="posts_list"),
+    path('<slug:post_slug>/<int:post_id>', views.post_detail, name="post_detail"),
 ]
 
 
--- src/forum/views.py
+++ src/forum/views.py
@@ -6,3 +6,8 @@ def posts_list(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'forum/posts_list.html', context)
+
+def post_detail(request, post_slug, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'forum/post_detail.html', context)
