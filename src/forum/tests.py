ea6ce46092cd35d502290b7b6098fdfb4eb28e8c
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-29 15:43:13 +0100

    Updated forum.tests.py.

--- src/forum/tests.py
+++ src/forum/tests.py
@@ -2,4 +2,12 @@ from django.test import TestCase
 from .models import Post
 
 class PostTestCase(TestCase):
-    pass
+    def setUp(self):
+        Post.objects.create(
+            'title'='test_title',
+            'text'='my_text',
+        )
+
+    def test_title(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.title, 'test_title')
