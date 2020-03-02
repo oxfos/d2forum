a139e0901df9e92a9ee33e942b7fdadbc88839ce
oxfos <rinaldo_bertossa@hotmail.com>
2020-03-02 08:10:24 +0100

    Added some tests to forum.tests.test_urls.py.

--- src/forum/tests/test_urls.py
+++ src/forum/tests/test_urls.py
@@ -1,4 +1,24 @@
-from django.test import TestCase, client
+from django.test import TestCase, SimpleTestCase, client
+from django.urls import reverse, resolve
+from forum.views import posts_list, post_detail, new_post
+
+
+class ForumUrlTests(SimpleTestCase):
+    """Test forum urls."""
+    def test_list_url_is_resolved(self):
+        url = reverse('forum:posts_list')
+        res = resolve(url)
+        self.assertEqual(res.func, posts_list)
+
+    def test_post_detail_is_resolved(self):
+        url = reverse('forum:post_detail', kwargs={'post_id':1, 'post_slug': 'some-slug'})
+        res = resolve(url)
+        self.assertEqual(res.func, post_detail)
+
+    def test_new_post_url_is_resolved(self):
+        url = reverse('forum:new_post')
+        res = resolve(url)
+        self.assertEqual(res.func, new_post)
 
 
 class DirectUrlTests(TestCase):
