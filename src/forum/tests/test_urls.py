7184a0662adeca51f31490b10a1da3319093e854
oxfos <rinaldo_bertossa@hotmail.com>
2020-03-02 21:09:34 +0100

    Simplified forum tests.

--- src/forum/tests/test_urls.py
+++ src/forum/tests/test_urls.py
@@ -4,29 +4,41 @@ from forum.views import posts_list, post_detail, new_post
 
 
 class ForumUrlTests(SimpleTestCase):
-    """Test forum urls."""
-    def test_list_url_is_resolved(self):
+    """Test reverse and resolve urls."""   
+    databases = '__all__'
+    def test_post_list_url(self):
+        # Test link namespace > url > view function.
         url = reverse('forum:posts_list')
+        self.assertEqual(url, '/')
         res = resolve(url)
         self.assertEqual(res.func, posts_list)
-
-    def test_post_detail_is_resolved(self):
-        url = reverse('forum:post_detail', kwargs={'post_id':1, 'post_slug': 'some-slug'})
-        res = resolve(url)
-        self.assertEqual(res.func, post_detail)
-
-    def test_new_post_url_is_resolved(self):
-        url = reverse('forum:new_post')
-        res = resolve(url)
-        self.assertEqual(res.func, new_post)
-
-
-class DirectUrlTests(TestCase):
-    """ Testing the environment before creating model instances."""
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
+        # Test get request.
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_about_page_status_code(self):
-        response = self.client.get('/new_post/')
+    def test_post_detail_is_resolved(self):
+        # Test link namespace > url > view function.
+        url = reverse('forum:post_detail', kwargs={'post_id':1, 'post_slug': 'some-slug'})
+        self.assertEqual(url, '/1/some-slug')
+        res = resolve(url)
+        self.assertEqual(res.func, post_detail)
+        # Test get request.
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post_GET(self):
+        # Test namespace > url > view function.
+        url = reverse('forum:new_post')
+        self.assertEqual(url, '/new_post/')
+        res = resolve(url)
+        self.assertEqual(res.func, new_post)
+        # Test get request.
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+     
+    def test_new_post_POST(self):
+        # Test POST request to new_post works.
+        url = reverse('forum:new_post')
+        response = self.client.post(url, {'title': 'my post', 'text': 'whatever text'},
+         follow=True) # follow=True ensures redirect is followed
         self.assertEqual(response.status_code, 200)
