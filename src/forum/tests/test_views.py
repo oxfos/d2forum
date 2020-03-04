bf419643fb2ff82d41cc8cb746bb6335e583e323
oxfos <rinaldo_bertossa@hotmail.com>
2020-03-04 12:30:26 +0100

    Added view test in forum.tests.test_views.py.

--- src/forum/tests/test_views.py
+++ src/forum/tests/test_views.py
@@ -2,15 +2,20 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class TestPost_listView(TestCase):
+class TestPosts_listView(TestCase):
     """Test the post_list view."""
     def setUp(self):
         # Prepare a response object.        
         self.response = self.client.get('/')
 
-    def test_post_list_view(self):
+    def test_posts_list_view_template(self):
         # Test that it returns the right template
         self.assertTemplateUsed(self.response, 'forum/posts_list.html')
-        
 
-    # that the templates contains a certian context
+    def test_posts_list_view_text_in_template(self):
+        # Test that a certain text is in the returned template.
+        self.assertContains(self.response, 'These')
+
+    def test_posts_list_view_context(self):
+        # Test that a certain term is in the response context.
+        self.assertIn('posts', self.response.context)
