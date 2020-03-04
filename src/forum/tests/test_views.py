fab27a8138985594369e115d633035802c015dd5
oxfos <rinaldo_bertossa@hotmail.com>
2020-03-04 11:52:20 +0100

    Removed super().setUp() from forum.tests.test_views.py.

--- src/forum/tests/test_views.py
+++ src/forum/tests/test_views.py
@@ -7,14 +7,10 @@ class TestPost_listView(TestCase):
     def setUp(self):
         # Prepare a response object.        
         self.response = self.client.get('/')
-        super().setUp()
 
     def test_post_list_view(self):
         # Test that it returns the right template
         self.assertTemplateUsed(self.response, 'forum/posts_list.html')
-    
-    def test_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-    
+        
 
     # that the templates contains a certian context
