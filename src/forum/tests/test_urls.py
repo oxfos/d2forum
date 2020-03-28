21e3847e5d0dfd80354d7ab9e5002e044c9a623f
oxfos <rinaldo_bertossa@hotmail.com>
2020-03-28 18:34:10 +0100

    Updated forum url tests.

--- src/forum/tests/test_urls.py
+++ src/forum/tests/test_urls.py
@@ -1,11 +1,15 @@
-from django.test import TestCase, SimpleTestCase, client
+from django.test import TestCase, client
 from django.urls import reverse, resolve
 from forum.views import posts_list, post_detail, new_post
+from ..models import Post
 
 
-class ForumUrlTests(SimpleTestCase):
-    """Test reverse and resolve urls."""   
-    databases = '__all__'
+class ForumUrlTests(TestCase):
+    """Test reverse and resolve urls."""
+    def setUp(self):
+        # Create Post object for test.
+        self.post = Post.objects.create(title='my title', text='my text')
+
     def test_post_list_url(self):
         # Test link namespace > url > view function.
         url = reverse('forum:posts_list')
@@ -18,13 +22,13 @@ class ForumUrlTests(SimpleTestCase):
 
     def test_post_detail_is_resolved(self):
         # Test link namespace > url > view function.
-        url = reverse('forum:post_detail', kwargs={'post_id':1, 'post_slug': 'some-slug'})
-        self.assertEqual(url, '/1/some-slug')
+        url = reverse('forum:post_detail', kwargs={'post_id':1, 'post_slug': 'my-title'})
+        self.assertEqual(url, '/1/my-title')
         res = resolve(url)
         self.assertEqual(res.func, post_detail)
         # Test get request.
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200) 
 
     def test_new_post_GET(self):
         # Test namespace > url > view function.
@@ -40,5 +44,6 @@ class ForumUrlTests(SimpleTestCase):
         # Test POST request to new_post works.
         url = reverse('forum:new_post')
         response = self.client.post(url, {'title': 'my post', 'text': 'whatever text'},
-         follow=True) # follow=True ensures redirect is followed
-        self.assertEqual(response.status_code, 200)
+         follow=False) # follow=True ensures redirect is followed
+        self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
