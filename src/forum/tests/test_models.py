cfcdcf29ec85bec29798418226c5ddbdbc6fc91d
oxfos <rinaldo_bertossa@hotmail.com>
2020-03-29 21:52:26 +0200

    Added new tests for forum models.

--- src/forum/tests/test_models.py
+++ src/forum/tests/test_models.py
@@ -18,3 +18,25 @@ class PostModelTest(TestCase):
         # Test slug.
         my_slug = slugify(self.post.title)
         self.assertEqual(self.post.slug, my_slug)
+    
+    def test_field_labels(self):
+        # Test title label.
+        title_label = self.post._meta.get_field('title').verbose_name
+        self.assertEqual(title_label, 'title')
+        # Test text label.
+        text_label = self.post._meta.get_field('text').verbose_name
+        self.assertEqual(text_label, 'text')
+        # Test date_added label.
+        date_added_label = self.post._meta.get_field('date_added').verbose_name
+        self.assertEqual(date_added_label, 'date added')
+
+    def test_field_max_lengths(self):
+        # Test title field max length.
+        title_length = self.post._meta.get_field('title').max_length
+        self.assertEqual(title_length, 200)
+
+    def test_post_object_name(self):
+        # Test that title is used as object name.
+        post = self.post
+        expected_object_name = self.post.title
+        self.assertEqual(expected_object_name, str(post))
