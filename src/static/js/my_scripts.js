c86a8840fe1946f53bffc45742cff2aa6d9ff817
oxfos <rinaldo_bertossa@hotmail.com>
2020-04-04 20:15:59 +0200

    Added part of js code to add reply to existing post.

--- src/static/js/my_scripts.js
+++ src/static/js/my_scripts.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-    console.log('this is a testo');
+
+
+// CSRF AJAX HEADER
+
+    var csrftoken = Cookies.get('csrftoken');
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+
+// EVENT TRIGGERS:
+
+    $('button[name="reply"]').click(submitReply);
+
+// EVENT HANDLERS:
+
+    function submitReply(event){
+        event.preventDefault();
+        var reply = $(this).val();
+        var data = $(this).closest('form').serialize();
+        $.ajax({
+            url: $(this).closest('form').attr('action'),
+            type: 'POST',
+            data: {
+                reply: reply
+            },
+            success: function(data){
+                console.log(data)
+            }
+        });
+    };
+
 });
